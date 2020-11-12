d2090077d286f7104656a95b1a4ab0b8fc36837f
Chandrika <chandrikadalakoti2@gmail.com>
2020-11-12 16:04:41 +0530

    module5

---
+++ after/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+$("#navbar-toggle").blur(function(event){
+    var screenwidth=window.innerWidth;
+    if(screenwidth<768)
+    {
+    $("#collapsable-nav").collapse('hide');
+    }
+    });
+}); 
