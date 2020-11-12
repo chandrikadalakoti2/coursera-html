69d8379d032dabe38f09db3238d5e56104683b9e
Chandrika <chandrikadalakoti2@gmail.com>
2020-11-12 16:35:49 +0530

    module5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage;
-      homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",chosenCategoryShortName);
+      homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",chosenCategoryShortName[0]);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
